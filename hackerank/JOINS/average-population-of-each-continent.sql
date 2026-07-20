eba424d18d63b932d447288033a7f0a8f0025076
Yousuf Ali Shakib <119650560+YousufSakib@users.noreply.github.com>
2026-07-20 23:56:34 +0600

    activity commit

--- hackerank/JOINS/average-population-of-each-continent.sql
+++ hackerank/JOINS/average-population-of-each-continent.sql
@@ -4,7 +4,6 @@ Problem Link:
 
     https://www.hackerrank.com/challenges/average-population-of-each-continent/problem?isFullScreen=true
 
-
 Problem Statement:
 
     Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
